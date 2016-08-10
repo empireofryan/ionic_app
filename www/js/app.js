b773697920c56334b32ddcf11341f1fce0c69cd1
empireofryan <rjohnson2001@gmail.com>
2016-08-10 04:50:16 -0400

    starred entries and refresh on scroll up

--- www/js/app.js
+++ www/js/app.js
@@ -32,6 +32,17 @@ angular.module('starter', ['ionic'])
         $scope.artists.splice($scope.artists.indexOf(item), 1);
       }
 
+      $scope.doRefresh = function() {
+        $http.get('js/data.json').success(function(data) {
+          $scope.artists = data;
+          $scope.$broadcast('scroll.refreshComplete')
+        });
+      }
+
+      $scope.toggleStar = function(item) {
+        item.star = !item.star;
+      }
+
       $scope.moveItem = function(item, fromIndex, toIndex) {
         $scope.artists.splice(fromIndex, 1);
         $scope.artists.splice(toIndex, 0, item);
